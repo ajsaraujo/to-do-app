18fcf8c08821e8248acc950ad499833321d13477
Allan <allanjuansil@gmail.com>
2020-04-30 13:19:55 -0300

    Sort toDos on refresh

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
   List _toDoList = [];
+  Map<String, dynamic> _lastRemovedToDo;
+  int _lastRemovedToDoIndex;
   final newToDoController = TextEditingController();
 
-  @override 
+  @override
   void initState() {
     super.initState();
-    
+
     _readToDosFromFile().then((jsonData) {
       setState(() {
         _toDoList = json.decode(jsonData);
       });
     });
   }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -63,26 +65,75 @@ class _HomeState extends State<Home> {
     }
   }
 
-  Widget _buildItem(context, index) {
-    return CheckboxListTile(
-      title: Text(_toDoList[index]['title']),
-      value: _toDoList[index]['done'],
-      secondary: CircleAvatar(
-        backgroundColor: _toDoList[index]['done'] ? Colors.blueAccent : Colors.red,
-        child: Icon(
-          _toDoList[index]['done'] ? Icons.check : Icons.clear,
-          color: Colors.white,
-        ),
-      ),
-      onChanged: (done) {
-        setState(() {
-          _toDoList[index]['done'] = done;
-          _saveToDosToFile();
-        });
-      }
-    );
+  Future<Null> _sortToDos() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((toDoA, toDoB) {
+        if (toDoA['done'] == toDoB['done']) return 0;
+        if (!toDoA['done']) return -1;
+        return 1;
+      });
+
+      _saveToDosToFile();
+    });
   }
-  
+
+  Widget _buildItem(context, index) {
+    return Dismissible(
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+              alignment: Alignment(-0.9, 0.0),
+              child: Icon(Icons.delete_forever, color: Colors.white)),
+        ),
+        direction: DismissDirection.startToEnd,
+        onDismissed: (direction) {
+          setState(() {
+            _lastRemovedToDo = Map.from(_toDoList[index]);
+            _lastRemovedToDoIndex = index;
+            _toDoList.removeAt(index);
+            _saveToDosToFile();
+
+            final undoSnackBar = SnackBar(
+                content: Text('To do "${_lastRemovedToDo['title']}" removed',
+                    style: TextStyle(color: Colors.white)),
+                backgroundColor: Colors.blueAccent,
+                duration: Duration(seconds: 3),
+                action: SnackBarAction(
+                    textColor: Colors.white,
+                    label: 'UNDO',
+                    onPressed: () {
+                      setState(() {
+                        _toDoList.insert(
+                            _lastRemovedToDoIndex, _lastRemovedToDo);
+                        _saveToDosToFile();
+                      });
+                    }));
+
+            Scaffold.of(context).showSnackBar(undoSnackBar);
+          });
+        },
+        child: CheckboxListTile(
+            title: Text(_toDoList[index]['title']),
+            value: _toDoList[index]['done'],
+            secondary: CircleAvatar(
+              backgroundColor:
+                  _toDoList[index]['done'] ? Colors.blueAccent : Colors.red,
+              child: Icon(
+                _toDoList[index]['done'] ? Icons.check : Icons.clear,
+                color: Colors.white,
+              ),
+            ),
+            onChanged: (done) {
+              setState(() {
+                _toDoList[index]['done'] = done;
+                _saveToDosToFile();
+              });
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,11 +167,12 @@ class _HomeState extends State<Home> {
               ),
             ),
             Expanded(
-                child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: _buildItem
-             ) )
+                child: RefreshIndicator(
+                    onRefresh: _sortToDos,
+                    child: ListView.builder(
+                        padding: EdgeInsets.only(top: 10.0),
+                        itemCount: _toDoList.length,
+                        itemBuilder: _buildItem)))
           ],
         ));
   }
