ce8e1727bf01fb83e0c16e580a5080ef451b9238
Allan <allanjuansil@gmail.com>
2020-05-04 20:30:54 -0300

    Create views folder and separate home page from main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:todo_list/widgets/input_section.dart';
-import 'package:todo_list/widgets/to_do_list_view.dart';
-
-import 'controllers/file_controller.dart';
-import 'controllers/to_do_controller.dart';
+import 'package:todo_list/views/home.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
     ));
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final textFieldController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    FileController.readToDosFromFile().then((jsonData) {
-      setState(() {
-        ToDoController.toDoList = json.decode(jsonData);
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('To Dos'),
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-              child: InputSection((String newToDoTitle) {
-                setState(() {
-                  ToDoController.addToDo(newToDoTitle);
-                });
-              }),
-            ),
-            ToDoListView(),
-          ],
-        ));
-  }
-}
---
+++ lib/views/home.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list/controllers/file_controller.dart';
+import 'package:todo_list/controllers/to_do_controller.dart';
+import 'package:todo_list/widgets/input_section.dart';
+import 'package:todo_list/widgets/to_do_list_view.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final textFieldController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    FileController.readToDosFromFile().then((jsonData) {
+      setState(() {
+        ToDoController.toDoList = json.decode(jsonData);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('To Dos'),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: InputSection((String newToDoTitle) {
+                setState(() {
+                  ToDoController.addToDo(newToDoTitle);
+                });
+              }),
+            ),
+            ToDoListView(),
+          ],
+        ));
+  }
+}
