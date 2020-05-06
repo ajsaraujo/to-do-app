543d0bacba7c079549362b4a010804611c67ae6a
Allan <allanjuansil@gmail.com>
2020-05-05 21:32:42 -0300

    Show ColorPicker on an AlertDialog

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -4,22 +4,59 @@ import 'package:todo_list/widgets/my_app_bar.dart';
 
 class SettingsPage extends StatefulWidget {
   Function changeThemeCallback;
-  
+
   SettingsPage({this.changeThemeCallback});
-  
+
   @override
   _SettingsPageState createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  ColorSwatch _appColor; 
-  
+  ColorSwatch _appColor = Colors.blue;
+
+  void _showColorPickerDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            contentPadding: const EdgeInsets.all(6.0),
+            title: Text('Pick a color'),
+            content: Container(height: 250.0, child: MaterialColorPicker()),
+            actions: [
+              FlatButton(
+                  child: Text('CANCEL', style: TextStyle(color: Colors.blue)),
+                  onPressed: Navigator.of(context).pop),
+              FlatButton(
+                  child: Text('OK', style: TextStyle(color: Colors.blue)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  })
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'Settings', hasReturnButton: true),
-      body: Center(
-        child: MaterialColorPicker(
+        appBar: MyAppBar(title: 'Settings', hasReturnButton: true),
+        body: Center(
+            child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Main color'),
+              trailing: Material(
+                shape: const CircleBorder(),
+                child: CircleAvatar(
+                  backgroundColor: _appColor,
+                  radius: 16,
+                ),
+              ),
+              onTap: _showColorPickerDialog,
+            ),
+          ],
+        )
+            /*child: MaterialColorPicker(
           allowShades: false,
           onMainColorChange: (ColorSwatch color) {
             setState(() {
@@ -27,8 +64,7 @@ class _SettingsPageState extends State<SettingsPage> {
               this.widget.changeThemeCallback(newPrimaryColor: color);
             });
           },
-        )
-      )
-    );
+        )*/
+            ));
   }
 }
